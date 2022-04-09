7ba4db68ccb1a1452bf7a343915327402fb2d63c
z.salikhkulova <z.salikhkulova@fun-box.ru>
2022-04-09 14:28:20 +0500

    Add TranslateModule to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { TranslateModule } from '@ngx-translate/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -46,6 +48,7 @@ import { TagsComponent } from './tags/tags.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    TranslateModule.forRoot(),
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
