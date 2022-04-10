e0b2c3a448fbcb26c70d49b194f79492e4d186be
z.salikhkulova <z.salikhkulova@fun-box.ru>
2022-04-10 21:23:06 +0500

    Remove unused import

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import locationParams from 'src/helpers/location-params';
 
