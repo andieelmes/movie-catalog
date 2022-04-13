5a755288cc2ff5daea6b59780b2d6a3bae672910
z.salikhkulova <z.salikhkulova@fun-box.ru>
2022-04-13 23:43:15 +0500

    Add language switcher test

--- src/app/language-switcher/language-switcher.component.spec.ts
+++ src/app/language-switcher/language-switcher.component.spec.ts
@@ -1,25 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslateService } from '@ngx-translate/core';
 
 import { LanguageSwitcherComponent } from './language-switcher.component';
+
+class MockTranslateService {
+  currentLang = 'en';
+  use = () => {}
+}
 
 describe('LanguageSwitcherComponent', () => {
   let component: LanguageSwitcherComponent;
   let fixture: ComponentFixture<LanguageSwitcherComponent>;
+  let translateService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LanguageSwitcherComponent ]
+      declarations: [LanguageSwitcherComponent],
+      providers: [LanguageSwitcherComponent, { provide: TranslateService, useClass: MockTranslateService }],
+      imports: [MatButtonModule]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
+    translateService = TestBed.inject(TranslateService);
     fixture = TestBed.createComponent(LanguageSwitcherComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should set current lang based on translation service', () => {
+    component.ngOnInit();
+    expect(component.currentLang).toBe('en');
+  });
+
+  it('should update current language', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('button[data-language="ru"]');
+    expect(button).toBeTruthy();
+    button.click();
+
+    expect(component.currentLang).toBe('ru');
+    expect(localStorage.getItem('_current_language')).toBe('ru');
   });
 });
