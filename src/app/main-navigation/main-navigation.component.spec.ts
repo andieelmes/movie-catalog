02d7f17dff12c3a44f7d09b4af36d3972d138d9e
z.salikhkulova <z.salikhkulova@fun-box.ru>
2022-04-13 23:43:40 +0500

    Add main navigation test

--- src/app/main-navigation/main-navigation.component.spec.ts
+++ src/app/main-navigation/main-navigation.component.spec.ts
@@ -1,25 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateTestingModule } from 'ngx-translate-testing';
+import { NavigationEnd, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { SharedModule } from 'src/app/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MainNavigationComponent } from './main-navigation.component';
+
+new NavigationEnd(0, '/home', '/favorites')
+
+class MockRouter {
+  url = '/';
+  events = of(new NavigationEnd(0, '/favorites', '/favorites'));
+}
 
 describe('MainNavigationComponent', () => {
   let component: MainNavigationComponent;
   let fixture: ComponentFixture<MainNavigationComponent>;
+  let router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainNavigationComponent ]
+      imports: [
+        TranslateTestingModule
+          .withTranslations({})
+          .withDefaultLanguage('en'),
+        SharedModule,
+        BrowserAnimationsModule,
+      ],
+      declarations: [MainNavigationComponent],
+      providers: [MainNavigationComponent,
+        { provide: Router, useClass: MockRouter },
+      ],
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(MainNavigationComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should set current active url', () => {
+    expect(component.activeItemUrl).toBe('/');
+  });
+
+  it('should set active url on change', () => {
+    fixture.detectChanges();
+
+    expect(component.activeItemUrl).toBe('/favorites');
   });
 });
